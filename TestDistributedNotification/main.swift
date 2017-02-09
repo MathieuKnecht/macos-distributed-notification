e76c7bfc9516d6f33abe235b59e5e2635ba527b0
Mathieu Knecht <mathieuknecht@Mathieus-Mac-mini.local>
2017-02-09 12:48:21 +0100

    Initial Commit

---
+++ TestDistributedNotification/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TestDistributedNotification
+//
+//  Created by Mathieu Knecht on 09.02.17.
+//  Copyright © 2017 Mathieu Knecht. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
